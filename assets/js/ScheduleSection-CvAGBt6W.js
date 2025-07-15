1d5ccc178dbbb61e3529a83c39e97ae32b114c37
IvanMitska <IvanMitska@users.noreply.github.com>
2025-07-15 07:38:26 +0000

    deploy: 49e7979093ae5f7d4f66ca3b34673f14367aff9b

--- assets/js/ScheduleSection-CvAGBt6W.js
+++ assets/js/ScheduleSection-CvAGBt6W.js
@@ -1,4 +1,4 @@
-import{a as e,r,j as a}from"./vendor-react-CiYCyw2Q.js";import{d as t}from"./vendor-styles-BbzM21L4.js";import{S as i,C as n,a as o}from"./SportsPage-zzLNfaep.js";import"./vendor-other-CZupzcPt.js";import"./main-3rwIPTLW.js";import"./vendor-i18n-C4kh69yx.js";import"./vendor-animations-BNNrahxh.js";import"./PageScrollReset-BkeoSiAQ.js";const d=t.div`
+import{a as e,r,j as a}from"./vendor-react-CiYCyw2Q.js";import{d as t}from"./vendor-styles-BbzM21L4.js";import{S as i,C as n,a as o}from"./SportsPage-DdNiZWS9.js";import"./vendor-other-CZupzcPt.js";import"./main--8uuguXI.js";import"./vendor-i18n-C4kh69yx.js";import"./vendor-animations-BNNrahxh.js";import"./PageScrollReset-BkeoSiAQ.js";const d=t.div`
   width: 100%;
   margin: 2rem 0;
   padding: 1.5rem;
