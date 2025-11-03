27d083522684e912922903df626f1860313b0856
IvanMitska <IvanMitska@users.noreply.github.com>
2025-11-03 10:00:05 +0000

    deploy: e7d7273fc6abda32649570410ce2233f50eeb8bd

--- assets/js/FacilitySection-CH-UzoHH.js
+++ assets/js/FacilitySection-CH-UzoHH.js
@@ -1,4 +1,4 @@
-import{a as e,r as i,b as t,j as r,U as a,ak as s,f as o,an as n}from"./vendor-react-FHR7Sy-E.js";import{d as l}from"./vendor-styles-DcQt-q6S.js";import{m as p}from"./vendor-animations-BsFJxBmq.js";import{S as c,T as d,C as x,a as h,b as g,c as m,B as f}from"./SportsPage-D1ei492s.js";import"./vendor-other-CZupzcPt.js";import"./main-CDwbBpxC.js";import"./vendor-i18n-C4kh69yx.js";import"./PageScrollReset-Jvg3lmV2.js";const b=l(p.div)`
+import{a as e,r as i,b as t,j as r,U as a,ak as s,f as o,an as n}from"./vendor-react-FHR7Sy-E.js";import{d as l}from"./vendor-styles-DcQt-q6S.js";import{m as p}from"./vendor-animations-BsFJxBmq.js";import{S as c,T as d,C as x,a as h,b as g,c as m,B as f}from"./SportsPage-B7KFJZIb.js";import"./vendor-other-CZupzcPt.js";import"./main-D1WEVxpb.js";import"./vendor-i18n-C4kh69yx.js";import"./PageScrollReset-Jvg3lmV2.js";const b=l(p.div)`
   display: grid;
   grid-template-columns: 1fr 1fr;
   gap: 4rem;
