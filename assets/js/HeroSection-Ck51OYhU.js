8cf9bc70929ea2237c107611138089af4cdfae43
IvanMitska <IvanMitska@users.noreply.github.com>
2026-02-06 17:37:30 +0000

    deploy: 2a49ebf266616595eda49b70e21a46413c8b8520

--- assets/js/HeroSection-Ck51OYhU.js
+++ assets/js/HeroSection-Ck51OYhU.js
@@ -82,12 +82,12 @@ import{r as e,j as t}from"./react-core-DWAaPJsY.js";import{d as i}from"./styled-
   text-align: center;
 
   @media (max-width: 768px) {
-    font-size: clamp(3.5rem, 16vw, 6rem);
+    font-size: clamp(2.5rem, 11vw, 5rem);
     line-height: 1.05;
   }
 
   @media (max-width: 480px) {
-    font-size: clamp(3rem, 18vw, 5rem);
+    font-size: clamp(2rem, 10vw, 4rem);
   }
 `,d=i.span`
   display: block;
